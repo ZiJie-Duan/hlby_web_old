de12e34336203bafe32f554b2a6c4334f3c7d2d2
lucycore <1296720721@qq.com>
2019-03-04 20:45:49 +0800

    基本

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+#from mods import paswd
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'haileytianjinlucycore'
+
+
+#定义服务器模型
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True)
+	users = db.relationship('Pas', backref='username')
+
+	def __repr__(self):
+		return '<Role %r>' % self.username
+
+
+class Pas(db.Model):
+	__tablename__ = 'passwords'
+	id = db.Column(db.Integer, primary_key=True)
+	password = db.Column(db.String(64), unique=True, index=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.password
+'''
+with open("test.json") as zx:
+	tssss = json.load(zx)
+'''
